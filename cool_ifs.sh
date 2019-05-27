4c9e693bc303e60959fd2c6ad8aee01397558915
Ed Mamerto <edmamerto@gmail.com>
2019-05-27 01:57:08 -0400

    add cool_ifs

---
+++ cool_ifs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if cmp a b $> /dev/null
+then
+    echo "Files a and b are identical."
+else
+    echo "Files a and b differ"
+
+if grep -q Bash file
+    then echo "File contains at least one occurence of Bash"
+fi
+
+word=Linux
+letter_sequence=inu
+
+if echo "$word" | grep - -q "$letter_sequence"
+then
+    echo "$letter_sequence found in $word"
+else
+    echo "$letter_sequence not found in $word"
+fi
+
+
